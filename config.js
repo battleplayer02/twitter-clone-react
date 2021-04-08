7e9b593d633271d2770988ce08ef1154fe69914d
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-04-08 18:24:32 +0530

    a

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://himanshu:vkxR1i4ub7zIKu5p@cluster0.8cgfe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
